8e246724877fc79f563f57fc43a6b2d8440998f7
Jaehoon Jang <07jjh1129@naver.com>
2025-07-31 21:01:55 +0900

    .

--- finagent/bin/pwiz.py
+++ finagent/bin/pwiz.py
@@ -1,4 +1,4 @@
-#!/Users/shinyjay/Desktop/finagent/finagent/bin/python3.13
+#!/Users/shinyjay/Desktop/financial-agent/finagent/bin/python3.11
 
 import datetime
 import os
@@ -61,7 +61,7 @@ def print_models(introspector, tables=None, preserve_order=False,
     header = HEADER % (
         introspector.get_additional_imports(),
         introspector.get_database_class().__name__,
-        introspector.get_database_name(),
+        introspector.get_database_name().replace('\\', '\\\\'),
         ', **%s' % repr(db_kwargs) if db_kwargs else '')
     print_(header)
 
